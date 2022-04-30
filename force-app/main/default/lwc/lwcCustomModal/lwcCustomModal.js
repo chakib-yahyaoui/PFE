e65770f3497908634fe1befb4e120ea7624626c8
Shekib-yah <57287314+Shekib-yah@users.noreply.github.com>
2022-04-30 22:33:23 +0200

    ++++

--- force-app/main/default/lwc/lwcCustomModal/lwcCustomModal.js
+++ force-app/main/default/lwc/lwcCustomModal/lwcCustomModal.js
@@ -1,84 +1,106 @@
 import { LightningElement, wire, api, track} from 'lwc';
 import { refreshApex } from '@salesforce/apex';
+import createProject from '@salesforce/apex/projectListViewHelper.createProject';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
-import departmentObject from '@salesforce/schema/Project__c';
+import projectObject from '@salesforce/schema/Project__c';
 import getUserList from '@salesforce/apex/projectListViewHelper.getUserList';
+import getDepartmentList from '@salesforce/apex/projectListViewHelper.getDepartmentList';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
-import updateDepartment from '@salesforce/apex/departmentListViewHelper.updateDepartment';
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-
-
-
  
 export default class LwcCustomModal extends LightningElement {
+    
+  wiredProjects;
+  selectedProjects;
+  baseData;
+  @track customFormModal = false; 
+  @track projectRecoredId;
   @wire(getUserList) User;
   @track wiredgetUser;
-   @wire(getObjectInfo, { objectApiName: departmentObject })
-   departmentInfo;
-  @track isModalOpenEdit= false;
-    @api objectApiName; 
-    @track page = 1;
-    @track error; 
-    @track startingRecord = 1;
-    @track endingRecord = 0; 
-    @track pageSize = 15; 
-    @track totalRecountCount = 0;
-    @track items = []; 
-    @api NewDepartment ={};
-    @api department ={};
-    @track value;
-    @track wiredDataResult;
-    @api sortedDirection = 'asc';
-    @api sortedBy ;
-    @api searchKey = '';
-    @api recordId;
-    @track recordId ; 
-    @track data;
-    @track columns = columns;
-    @track allorgsData;
-    @track totalPage = 0;
-    hello= true;
-    result;
-    loading;
-    defaultSortDirection = 'asc';
-    @api OrgIdDet;
+  @wire(getDepartmentList) Department;
+  @track wiredgetDepartment;
+   @wire(getObjectInfo, { objectApiName: projectObject })
+   projectInfo;
 
-  openModalEdit() {
-    console.log('hi')
-    this.isModalOpenEdit = true; 
+   
+   @track isModalOpenEdit= false;
+   @api objectApiName; 
+   
+   @track error; 
+   @api NewProject ={};
+   @api project ={};
+   
+   @api projectObject={};
+
+ @track isModalOpen = false;
+openModal() {
+   this.isModalOpen = true; 
 }
-closeModalEdit() {
-    this.isModalOpenEdit = false;
+closeModal() {
+   this.isModalOpen = false;
 }
-handleNameEdit(event) {
-  this.NewDepartment.Name = event.target.value;
+handleNameChange(event) {
+  this.projectObject.Name = event.target.value;
 }
 
+handleSpecificationChange(event) {
+  this.projectObject.Specifications = event.target.value;
+}
+handleDatebeginChange(event) {
+ this.projectObject.Date_begin = event.target.value;
+}
+handleDateendChange(event) {
+ this.projectObject.Date_end = event.target.value;
+}
+handleMemberChange(event) {
+ this.projectObject.Members = event.target.value;
+}
+handleDepartmentChange(event) {
+  this.projectObject.Department = event.target.value;
+ }
+submitDetails() {
+   console.log('member',this.projectObject.Members)
+   console.log('manager',this.projectObject.Department)
+   createProject({ ProjectRecObj: this.projectObject })
+            .then(result => {
+              this.projectRecoredId = result.Id;
+              window.console.log('projectRecoredId##Vijay2 ' + this.projectRecoredId);  
+                console.log('success' + result);
+                this.isModalOpen = false;
+                this.projectObject = {};
+                this.dispatchEvent(
+                  new ShowToastEvent({
+                      title: 'Success',
+                      message: 'Department created successfully..!',
+                      variant: 'success',
+                  }),
+              );
+              refreshApex(this.wiredProjects);
+              this[NavigationMixin.Navigate]({
+                  type: 'standard__recordPage',
+                  attributes: {
+                      recordId: result.Id,
+                      objectApiName: 'Project__c',
+                      actionName: 'view'
+                  },
+              });
+                
 
-handleDescriptionEdit(event) {
-this.NewDepartment.Description__c = event.target.value;
-}
-handleMemberEdit(event) {
-this.NewDepartment.Member__c = event.target.value;
-}
-handleManagerEdit(event) {
-  this.NewDepartment.Manager__c = event.target.value;
-  }
-  handleProjectManagerEdit(event) {
-    this.NewDepartment.Project_Managers__c = event.target.value;
-    }
-    
-  submitDetailsEdit() {
- console.log('hi', this.NewDepartment.Member__c)
- console.log('hi', this.NewDepartment.Manager__c)
- console.log('hi', this.NewDepartment.Project_Managers__c)
-    updateOrg({ department: this.NewDepartment })
-        .then(() => {
-            console.log('tested',this.department);
-            this.NewDepartment = {}
-            console.log('teste',this.NewDepartment);
-            this.isModalOpenEdit = false;
-            refreshApex(this.wiredDataResult) });
-}
+             })
+             
+             
+             
+             .catch(error => {
+              this.dispatchEvent(
+                  new ShowToastEvent({
+                      title: 'Error creating record',
+                      message: error.body.message,
+                      variant: 'error',
+                  }),
+              );
+          });
+      
+            
+        }
+   
 }
---
+++ force-app/main/default/lwc/projectForDepartment/projectForDepartment.js
@@ -0,0 +1,53 @@
+import { LightningElement, wire, api } from "lwc";
+import getProjects from "@salesforce/apex/ProjectController.getProjects";
+import { refreshApex } from "@salesforce/apex";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import updateProjects from "@salesforce/apex/ProjectController.updateProjects";
+import { getRecordNotifyChange } from "lightning/uiRecordApi";
+
+const COLS = [
+    { label: "Name", fieldName: "Name", editable: true },
+    { label: "Specifications", fieldName: "Specifications__c", editable: true },
+    { label: "Date Begin", fieldName: "Date_begin__c", editable: true },
+    { label: "Date End", fieldName: "Date_end__c",editable: true  },
+  ];
+export default class ProjectForDepartment extends LightningElement {
+    @api recordId;
+    columns = COLS;
+    draftValues = [];
+  
+    @wire(getProjects, { depId: "$recordId" })
+    project;
+  
+    async handleSave(event) {
+      const updatedFields = event.detail.draftValues;
+  
+      // Prepare the record IDs for getRecordNotifyChange()
+      const notifyChangeIds = updatedFields.map((row) => {
+        return { recordId: row.Id };
+      });
+      console.log("###RecordIds : " + JSON.stringify(notifyChangeIds));
+      try {
+        const result = await updateProjects({ data: updatedFields });
+        console.log(JSON.stringify("Apex update result: " + result));
+        this.dispatchEvent(
+          new ShowToastEvent({
+            title: "Success",
+            message: "Project updated",
+            variant: "success"
+          })
+        );
+  
+        // Refresh LDS cache and wires
+        getRecordNotifyChange(notifyChangeIds);
+  
+        // Display fresh data in the datatable
+        refreshApex(this.project).then(() => {
+          // Clear all draft values in the datatable
+          this.draftValues = [];
+        });
+      } catch (error) {
+        console.log("###Error : " + JSON.stringify(error));
+      }
+    }
+}
