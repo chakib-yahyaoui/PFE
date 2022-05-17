35a753b01d78c2d7e7d70a9ad926166d26f3891d
Shekib-yah <57287314+Shekib-yah@users.noreply.github.com>
2022-05-17 17:46:20 +0200

    +++++++++++++++

--- force-app/main/default/lwc/opp_chartOpportunities/opp_chartOpportunities.js
+++ force-app/main/default/lwc/opp_chartOpportunities/opp_chartOpportunities.js
@@ -1,37 +1,25 @@
-import { LightningElement, wire } from 'lwc';
-import getOpportunities from '@salesforce/apex/GEN_ChartController.getOpportunitiees';
- 
-export default class Gen_opportunitychart extends LightningElement {
-    chartConfiguration;
- 
-    @wire(getOpportunities)
-    getOpportunities({ error, data }) {
-        if (error) {
-            this.error = error;
-            this.chartConfiguration = undefined;
-        } else if (data) {
-            let chartAmtData = [];
-            let chartLabel = [];
-            data.forEach(opp => {
-                chartLabel.push(opp.stage);
-            });
- 
-            this.chartConfiguration = {
-                type: 'pie',
-                data: {
-                    datasets: [{
-                            label: 'stage',
-                            backgroundColor: "#1190CB",
-                            data: chartAmtData,
-                        },
-                        
-                    ],
-                    labels: chartLabel,
-                },
-                options: {},
-            };
-            console.log('data => ', data);
-            this.error = undefined;
+import { LightningElement,api,wire,track } from 'lwc';
+import opportunityAggregation from '@salesforce/apex/OpportunityDataService.aggregateOpportunitiees'
+export default class Opp_chartOpportunities extends LightningElement {
+  @api recordId; //As the component is loaded on a account detail page, we will get access to the account Id.
+  @track opportunityData=[];//Chart accepts data in multiple formats, array, object etc. We are using array here. 
+  //get the data from the backend
+  @wire(opportunityAggregation,{}) opptyData({error, data}){
+    if(data){
+      let oppData = Object.assign({},data);
+      /*
+        this.opportunityData will in the below format
+        [['Closed Won',2],['Closed Lost',4],['Negotiation',5]];
+      */
+      for(let key in oppData){
+        if(oppData.hasOwnProperty(key)){
+          let tempData=[key, oppData[key]];
+          this.opportunityData.push(tempData);
         }
+      }
     }
+    if(error)
+      console.log(error);
+  }
+
 }
