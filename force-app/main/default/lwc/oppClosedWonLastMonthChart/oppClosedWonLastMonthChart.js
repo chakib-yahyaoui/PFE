2f868a8901d46165d0661c20f12f911d8f04757d
Shekib-yah <57287314+Shekib-yah@users.noreply.github.com>
2022-05-18 03:01:33 +0200

    ++++++++++

---
+++ force-app/main/default/lwc/oppClosedWonLastMonthChart/oppClosedWonLastMonthChart.js
@@ -0,0 +1,44 @@
+import {LightningElement, wire, track} from 'lwc';
+import getOpportunities from '@salesforce/apex/OpportunityChartCntrl.getOpportunities';
+
+export default class OppClosedWonLastMonthChart extends LightningElement {
+    @track chartConfiguration;
+
+ @wire(getOpportunities, {})
+ getOpportunities({error, data}) {
+  if (error) {
+   this.error = error;
+   console.log('error => ' + JSON.stringify(error));
+   this.chartConfiguration = undefined;
+  } else if (data) {
+   let chartData = [];
+   let chartLabels = [];
+   data.forEach(opp => {
+    chartData.push(opp.Amount);
+    chartLabels.push(opp.Name);
+   });
+
+   this.chartConfiguration = {
+    type: 'bar',
+    data: {
+     labels: chartLabels,
+     datasets: [
+      {
+       label: 'Closed Won Last Month',
+       barPercentage: 0.5,
+       barThickness: 6,
+       maxBarThickness: 8,
+       minBarLength: 2,
+                            backgroundColor: "orange",
+       data: chartData,
+      },
+     ],
+    },
+    options: {
+    },
+   };
+   console.log('data => ', data);
+   this.error = undefined;
+  }
+ }
+}
