d61a63d6c1a71798e092d5f6c28a8c65f325e596
Shekib-yah <chakib.yahyaoui1@esprit.tn>
2022-06-16 14:24:04 +0100

    +++++

--- force-app/main/default/lwc/deleteDep/deleteDep.js
+++ force-app/main/default/lwc/deleteDep/deleteDep.js
@@ -25,7 +25,7 @@ export default class DeleteDep extends LightningElement {
                 this[NavigationMixin.Navigate]({
                     type: 'standard__navItemPage',
                     attributes: {
-                        apiName: this.c/departmentListView
+                        apiName: this.departmentListView
                  },
              });
             })
